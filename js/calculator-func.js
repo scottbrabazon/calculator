4ad8b47c89d78c962235bea332a1f30e3c2c4ef9
Scott Brabazon <scott.brabazon@foundrycomms.co.uk>
2017-02-03 09:12:12 +0000

    Javascript in progress

---
+++ js/calculator-func.js
@@ -0,0 +1,189 @@
+// Calculator Function //
+
+var number = 2;
+
+// Numbers //
+
+function input0() {
+    var zero = document.getElementById("button0").value;
+    var number = (parseInt(zero));
+    console.log(parseInt(zero));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button0").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input1() {
+    var one = document.getElementById("button1").value;
+    var number = (parseInt(one));
+    console.log(parseInt(one));
+    document.write(number);
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button1").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input2() {
+    var two = document.getElementById("button2").value;
+    var number = (parseInt(two));
+    console.log(parseInt(two));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button2").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input3() {
+    var three = document.getElementById("button3").value;
+    var number = (parseInt(three));
+    console.log(parseInt(three));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button3").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input4() {
+    var four = document.getElementById("button4").value;
+    var number = (parseInt(four));
+    console.log(parseInt(four));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button4").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input5() {
+    var five = document.getElementById("button5").value;
+    var number = (parseInt(five));
+    console.log(parseInt(five));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button5").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input6() {
+    var six = document.getElementById("button6").value;
+    var number = (parseInt(six));
+    console.log(parseInt(six));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button6").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input7() {
+    var seven = document.getElementById("button7").value;
+    var number = (parseInt(seven));
+    console.log(parseInt(seven));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button7").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input8() {
+    var eight = document.getElementById("button8").value;
+    var number = (parseInt(eight));
+    console.log(parseInt(eight));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button8").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+function input9() {
+    var nine = document.getElementById("button9").value;
+    var number = (parseInt(nine));
+    console.log(parseInt(nine));
+    document.getElementById("placeholder").innerHTML = number;
+    if (document.getElementById("placeholder").innerHTML === 0) {
+        document.getElementById("placeholder").innerHTML = "";
+    }
+    var z = document.getElementById("placeholder").innerHTML;
+    var x = document.getElementById("button9").value;
+    document.getElementById("placeholder").innerHTML = parseInt(z + x);
+    var number = parseInt(z + x);
+}
+
+
+// Functions //
+
+function inputAC() {
+    var nine = document.getElementById("button9").value;
+    console.log(parseInt(nine));
+}
+
+function inputPlus() {
+    var zero = document.getElementById("button0").value;
+    console.log(0);
+}
+
+function input() {
+    var x = document.getElementById("button").value;
+    console.log(x);
+}
+
+
+// Functions //
+
+//var zero = document.getElementById("button0").value;
+//var one = document.getElementById("button1").value;
+//var two = document.getElementById("button2").value;
+//var three = document.getElementById("button3").value;
+//var four = document.getElementById("button4").value;
+//var five = document.getElementById("button5").value;
+//var six = document.getElementById("button6").value;
+//var seven = document.getElementById("button7").value;
+//var eight = document.getElementById("button8").value;
+//var nine = document.getElementById("button9").value;
+
+//console.log(x);
+
+
+
+
+
+
+
+
+
