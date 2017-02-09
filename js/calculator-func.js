53a7ff02a2765acdd76d9101c337ab706df31f40
Scott Brabazon <scott.brabazon@foundrycomms.co.uk>
2017-02-09 11:02:23 +0000

    Functions all correctly working

--- js/calculator-func.js
+++ js/calculator-func.js
@@ -1,194 +1,250 @@
 // Calculator Function //
 
-var oldNumber = 0;
-var number = 0;
-var sum = 0;
+
+// Variables //
+
+var input = [];
+var sum = [];
+var display = 0;
+var total = 0;
+
 
 // Numbers //
 
 function input0() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var zero = document.getElementById("button0").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + zero;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(0);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input1() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var one = document.getElementById("button1").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + one;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+	input.push(1);
+	display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input2() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var two = document.getElementById("button2").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + two;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(2);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input3() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var three = document.getElementById("button3").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + three;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(3);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input4() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var four = document.getElementById("button4").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + four;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(4);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input5() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var five = document.getElementById("button5").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + five;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(5);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input6() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var six = document.getElementById("button6").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + six;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(6);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input7() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var seven = document.getElementById("button7").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + seven;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    input.push(7);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input8() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
-    }
-    var eight = document.getElementById("button8").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + eight;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
-
+    input.push(8);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function input9() {
-    if (document.getElementById("placeholder").innerHTML === "0") {
-        document.getElementById("placeholder").innerHTML = "";
+    input.push(9);
+    display = input.join("");
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputDecimal() {
+    if (input.includes('.'))
+    {
+        input.push("");
     }
-    var nine = document.getElementById("button9").value;
-    var number = document.getElementById("placeholder").innerHTML;
-    var number = number + nine;
-    document.getElementById("placeholder").innerHTML = parseInt(number);
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("****************");
+    else
+    {
+        input.push(".");
+    }
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 
 // Functions //
 
 function inputAC() {
-    var number = 0;
-    console.log(parseInt(number));
-    document.getElementById("placeholder").innerHTML = number;
+    input = [];
+    oldNumber = 0;
+    sum = [];
+    display = 0;
+    previousNumber = 0;
+    total = 0;
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputNegative() {
+    input.push("-");
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputPercent() {
+    sum.push(display);
+    total = eval(sum.join(""));
+    sum = [];
+    sum.push(total);
+    sum.push("/100");
+    total = eval(sum.join(""));
+    display = total;
+    input = [];
+    document.getElementById("placeholder").innerHTML = display;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function inputPlus() {
-    var oldNumber = number;
-    var sum = parseInt(oldNumber) + parseInt(number);
-    document.getElementById("placeholder").innerHTML = sum;
-    console.log("Old Number" + " = ");
-    console.log(parseInt(oldNumber));
-    console.log("Current Number" + " = ");
-    console.log(parseInt(number));
-    console.log("Sum" + " = ");
-    console.log(parseInt(sum));
-    console.log("****************");
-    document.getElementById("placeholder").innerHTML = 0;
+    input.join("")
+    input = [];
+    sum.push(display);
+    sum.push("+");
+    display = 0;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputMinus() {
+    input = [];
+    sum.push(display);
+    sum.push("-");
+    display = 0;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputMultiply() {
+    input = [];
+    sum.push(display);
+    sum.push("*");
+    display = 0;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+}
+
+function inputDivide() {
+    input = [];
+    sum.push(display);
+    sum.push("/");
+    display = 0;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
 }
 
 function inputEquals() {
-    document.getElementById("placeholder").innerHTML = sum;
-    console.log("Old Number" + " = " + oldNumber);
-    console.log("Current Number" + " = " + number);
-    console.log("Sum" + " = " + sum);
-    console.log("****************");
+    sum.push(display);
+    total = eval(sum.join(""));
+    display = eval(sum.join(""));
+    sum = [];
+    input = [];
+    previousNumber = 0;
+    document.getElementById("placeholder").innerHTML = total;
+    console.log("input = " + input);
+    console.log("display = " + display);
+    console.log("sum = " + sum);
+    console.log("total = " + total);
+    console.log("********************");
+    total = 0;
 }
-
-
-// Functions //
-
-//var zero = document.getElementById("button0").value;
-//var one = document.getElementById("button1").value;
-//var two = document.getElementById("button2").value;
-//var three = document.getElementById("button3").value;
-//var four = document.getElementById("button4").value;
-//var five = document.getElementById("button5").value;
-//var six = document.getElementById("button6").value;
-//var seven = document.getElementById("button7").value;
-//var eight = document.getElementById("button8").value;
-//var nine = document.getElementById("button9").value;
-
-//console.log(x);
-
-
-
-
-
-
-
 
 
